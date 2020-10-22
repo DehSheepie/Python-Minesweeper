8316d70380bd4efd72489f6a10cced17148b4fe4
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-10-22 23:05:58 +0100

    Added difficulty settings.

--- venv/minesweeper.py
+++ venv/minesweeper.py
@@ -1,40 +1,38 @@
 from tkinter import *
 from game import Game
 
-size = 5
-mines = 5
-buttons = []
-left_or_right = 0   # 0 is left, 1 is right
 
-window = Tk()
-window.title("Minesweeper")
+
 
 class GameWindow:
     def __init__(self):
-        self.size = 5
-        self.mines = 5
+        self.window = Tk()
+        self.mines = 0
+        self.size = 0
+        self.window.title("Minesweeper")
+        self.set_difficulty_easy()
         self.buttons = []
-        self.game = Game(size)
-        self.game.add_mines(mines)
+        self.game = Game(self.size)
+        self.game.add_mines(self.mines)
         self.game.board_setup()
         self.game.console_display_board()
         self.game.console_display_mask()
 
     def setup_board(self):
         count = 0
-        for button in range(size * 5):
+        for button in range(self.size * self.size):
             # button = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace")
             # button.configure(command=lambda pass_button=button: b_click(w, pass_button))
             # button.bind('Button-1>', left)
             # button.bind('<Button-3>', right)
-            button = Label(window, width=8, height=4, background="gray")
+            button = Label(self.window, width=8, height=4, background="gray")
             self.buttons.insert(0, button)
             count += 1
 
         row = 0
         col = 0
         for button in self.buttons:
-            if col >= size:
+            if col >= self.size:
                 col = 0
                 row += 1
             button.grid(row=row, column=col)
@@ -48,7 +46,7 @@ class GameWindow:
         row = 0
         col = 0
         for button in self.buttons:
-            if col >= size:
+            if col >= self.size:
                 col = 0
                 row += 1
             button.grid(row=row, column=col)
@@ -64,19 +62,19 @@ class GameWindow:
             col += 1
 
     def create_menu(self):
-        menu = Menu(window, tearoff=0)
+        menu = Menu(self.window, tearoff=0)
 
         # Set up game menu
         game_menu = Menu(menu, tearoff=0)
-        game_menu.add_command(label='Restart', command=lambda b=board: self.restart_game())
+        game_menu.add_command(label='Restart', command=self.restart_game)
         game_menu.add_command(label='Auto-hint')
         game_menu.add_command(label='Hint')
 
         # Set up difficulty menu
         difficulty_menu = Menu(menu, tearoff=0)
-        difficulty_menu.add_command(label="Easy")
-        difficulty_menu.add_command(label="Medium")
-        difficulty_menu.add_command(label="Hard")
+        difficulty_menu.add_command(label="Easy", command=self.set_difficulty_easy)
+        difficulty_menu.add_command(label="Medium", command=self.set_difficulty_medium)
+        difficulty_menu.add_command(label="Hard", command=self.set_difficulty_hard)
 
         # Set up help menu
         help_menu = Menu(menu, tearoff=0)
@@ -88,9 +86,51 @@ class GameWindow:
         # Add difficulty menu to the main menu
         menu.add_cascade(label="Difficulty", menu=difficulty_menu)
 
-        menu.add_command(label='Exit', command=window.quit)
+        menu.add_command(label='Exit', command=self.window.quit)
 
-        window.config(menu=menu)
+        self.window.config(menu=menu)
+
+    def set_difficulty_hard(self):
+        self.size = 10
+        self.mines = 35
+        for widget in self.window.winfo_children():
+            widget.destroy()
+        self.create_menu()
+        self.buttons = []
+        self.game = Game(self.size)
+        self.game.add_mines(self.mines)
+        self.game.board_setup()
+        self.game.console_display_board()
+        self.game.console_display_mask()
+        self.setup_board()
+
+    def set_difficulty_medium(self):
+        self.size = 5
+        self.mines = 10
+        for widget in self.window.winfo_children():
+            widget.destroy()
+        self.create_menu()
+        self.buttons = []
+        self.game = Game(self.size)
+        self.game.add_mines(self.mines)
+        self.game.board_setup()
+        self.game.console_display_board()
+        self.game.console_display_mask()
+        self.setup_board()
+
+    def set_difficulty_easy(self):
+        self.size = 4
+        self.mines = 5
+        for widget in self.window.winfo_children():
+            widget.destroy()
+        self.create_menu()
+        self.buttons = []
+        self.game = Game(self.size)
+        self.game.add_mines(self.mines)
+        self.game.board_setup()
+        self.game.console_display_board()
+        self.game.console_display_mask()
+        self.setup_board()
 
     def restart_game(self):
         self.game.reset_game(self.size, self.mines)
@@ -117,4 +157,4 @@ board.create_menu()
 
 # Add this in once i make an icon --> window.iconbitmap('file path')
 
-window.mainloop()
+board.window.mainloop()
